8dab2edacfd00a2f72abf9eaa00b7612c236884a
Arthur K <arthurk031@outlook.com>
2024-08-29 16:28:10 -0300

    Início

---
+++ Calculos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Produto
+{
+    class Calculos
+    {
+        public string nome;
+        public double preco;
+        public int qnt;
+        public double CalcularPreco(double preco, int qnt)
+        {
+            if (qnt < 10)
+            {
+                return preco;
+            }
+            else if (qnt > 10 && qnt < 21)
+            {
+                return preco - (preco * 0.1);
+            }
+            else if (qnt > 20 && qnt < 51)
+            {
+                return preco - (preco * 0.2);
+            }
+            else
+            {
+                return preco - (preco * 0.25);
+            }
+        }
+        public Calculos(string nomeProduto, double precoProduto, int quantidade)
+        {
+            nome = nomeProduto;
+            preco = precoProduto;
+            qnt = quantidade;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Produto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao sistema de produtos!\n\nInsira, linha por linha, o nome do produto, seu preço e quantidade:");
+            string nome = Console.ReadLine();
+            double preco = double.Parse(Console.ReadLine());
+            int qnt = int.Parse(Console.ReadLine());
+            if (qnt > 0 && preco > 0)
+            {
+                Calculos p = new Calculos(nome, preco, qnt);
+                Console.WriteLine($"Produto - {p.nome};\nPreço total para {p.qnt} produto(s) - {p.CalcularPreco(preco, qnt)}");
+            }
+        }
+    }
+}
